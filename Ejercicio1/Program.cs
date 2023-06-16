a84983aa8543a07ae93618b92505a417023e737b
VelizAndres <dandresveliz18@gmail.com>
2023-06-16 10:35:18 -0600

    Primer Ejercicio

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir = Directory.GetCurrentDirectory();
+            Console.WriteLine(dir);
+            string ruta = dir+ "//..//..//..//data.txt";
+            List<Rect> Datos = new List<Rect>();
+            try
+            {
+                using (var sr = new StreamReader(ruta))
+                {
+                    string[] texto = sr.ReadToEnd().Split("\n");
+                    for(int i=0; i < texto.Length; i++)
+                    {
+                      if(texto[i]!="")
+                        {
+                      Rect nuevo = new Rect(texto[i]);
+                      Datos.Add(nuevo);
+                        }
+                    }
+                }
+             
+        }
+            catch (IOException e)
+            {
+                Console.WriteLine("Errores en el archivo");
+                Console.WriteLine(e.Message);
+            }
+
+            //Suma de Areas
+            int sum_area = 0;
+            foreach( Rect cuadro in Datos)
+            {
+                sum_area += cuadro.Area();
+            }
+            //Diferencia de Areas
+            int dif_area = 0;
+            foreach (Rect cuadro1 in Datos)
+            {
+                foreach (Rect cuadro2 in Datos)
+                {
+                        dif_area += cuadro1.Area_Dif(cuadro2);
+                }
+            }
+
+            // Resultado
+            int area_total = sum_area - dif_area;
+            Console.WriteLine("Area Total = "+ area_total);
+
+        }
+
+
+
+
+    }
+}
---
+++ Ejercicio1/Rectangulo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+public class Rect
+{
+	public int x1;
+	public int x2;
+	public int y1;
+	public int y2;
+
+	public Rect(string line)
+	{
+		string[] valores = line.Split(",");
+		x1 = Convert.ToInt32(valores[0]);
+		y1 = Convert.ToInt32(valores[1]);
+		x2 = Convert.ToInt32(valores[2]);
+		y2 = Convert.ToInt32(valores[3]);
+	}
+
+
+	public int Area()
+    {
+	int r_base = Math.Abs(x2 - x1);
+	int r_altura = Math.Abs(y2 - y1);
+		return r_altura * r_base;
+	}
+
+	public int Area_Dif(Rect rect)
+    {
+	      int dif_x = 0;
+			int dif_y = 0;
+		if (!(x1 == rect.x1 && x2 == rect.x2 && y1 == rect.y1 && y2 == rect.y2))
+			{
+			if (rect.x1 <= x1 && x1 <= rect.x2)
+			{
+				if (x2 <= rect.x2)
+				{
+					dif_x = x2 - x1;
+				}
+				else
+				{
+					dif_x = rect.x2 - x1;
+				}
+				if (rect.y1 <= y1 && y1 <= rect.y2)
+				{
+					if (y2 <= rect.y2)
+					{
+						dif_y = y2 - y1;
+					}
+					else
+					{
+						dif_y = rect.y2 - y1;
+					}
+				}
+			}
+		}
+		return dif_x  * dif_y;
+    }
+
+
+}
